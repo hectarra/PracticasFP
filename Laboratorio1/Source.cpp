50510bbf2103f9dd860fa4d81ab3eb4f66b03725
PabloRz <prabadan@ucm.es>
2017-10-24 17:42:48 +0200

    He subido mi primera sesion d elaboratorio de fundamentos de programacion a git hub.

---
+++ Laboratorio1/Source.cpp
@@ -0,0 +1,46 @@
+#include<iostream>
+using namespace std;
+
+int leernumentero();
+int sumdigitos(int ni);
+void mostrarresult(int ni, int resultado);
+
+int main()
+{
+	int ni;
+	int resultado;
+	ni = leernumentero();
+	resultado = sumdigitos(ni);
+	mostrarresult(ni, resultado);
+	system("pause");
+	return 0;
+}
+
+int leernumentero()
+{
+	int n;
+	cout << "Introduce un numero entero: " << endl;
+	cin >> n;
+	while (n < 0)
+		{
+			cout << "Introduce un numero entero: ";
+			cin >> n;
+		}
+	return n;
+}
+
+int sumdigitos(int ni)
+{
+	int suma = 0;
+	while (ni > 0)
+	{
+		suma = suma + ni % 10;
+		ni = ni / 10;
+	}
+	return suma;
+}
+
+void mostrarresult(int ni, int resultado)
+{
+	cout << "El resultado de la suma de los digitos de " << ni << " es " << resultado << endl;
+}
